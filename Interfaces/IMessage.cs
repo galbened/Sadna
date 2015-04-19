b253266aa5b965a0f9b0d9e689bb3406328d338f
tomerse <tomerse@post.bgu.ac.il>
2015-04-19 22:18:57 +0300

    Creating Interfaces project

--- Interfaces/IMessage.cs
+++ Interfaces/IMessage.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Message
+namespace Interfaces
 {
-     public interface IMessage
+    public interface IMessage
     {
         String getContent();
         String getPublisherName();
