72c76b35e106d8d3c18bfaaf32441c5d261767e4
poratGalpo <galpo@post.bgu.ac.il>
2015-04-20 00:20:21 +0300

    user MGMT + member Inter

--- User/userManagment.cs
+++ User/userManagment.cs
@@ -8,5 +8,12 @@ namespace User
 {
     interface userManagment
     {
+        Boolean login(String username, String password);
+        Boolean register(String username, String password);
+        Boolean enterForum(String forumName);// not sure that the right class for that
+        Boolean changePassword(String username, String oldPassword, String newPassword);
+        Boolean changeUsername(String oldUsername, String newUsername, String password);
+        Boolean addFriend(String friendID);
+
     }
 }
