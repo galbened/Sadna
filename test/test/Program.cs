96e683475803d2aa13a31ae7238e9c0d1db00c6f
galbened <galbened@post.bgu.ac.il>
2015-04-19 19:52:27 +0300

    code

---
+++ test/test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            for (; ;)
+            {
+                Console.WriteLine("Kishkush Mekushkash");
+                line = Console.ReadLine();
+                if (line == "q")
+                    break;
+            }
+
+        }
+    }
+}
