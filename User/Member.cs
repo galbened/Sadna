9dfeed2000bd2b9ea5213a351694960ddc5b9d6d
poratGalpo <galpo@post.bgu.ac.il>
2015-04-19 22:26:42 +0300

    new code

--- User/Member.cs
+++ User/Member.cs
@@ -8,6 +8,54 @@ namespace User
 {
     interface Member
     {
-        void logout();
+        String memberID;
+        String memberUsername;
+        private String memberPassword;
+        Boolean loggerStatus = false;
+
+        public Boolean login(String inputID, String inputPassowd)
+        {
+            if ((inputID.CompareTo(memberID) == 1) && (inputPassowd.CompareTo(memberPassword) == 1))
+            {
+                loggerStatus = false;
+                return true;
+            }
+            return false;
+        }
+
+
+        private void logout()
+        {
+            if (loggerStatus == true)
+                loggerStatus = false;
+        }
+
+        private Boolean changePassword(String oldPass, String newPass)
+        {
+            if (oldPass.CompareTo(memberPassword) == 1)
+            {
+                if (newPass.Length < 8) // depends on the the passwords strength
+                    return false;
+                memberPassword = newPass;
+                return true;
+            }
+            return false;
+        }
+
+        private Boolean changeUserName(String newUserName)
+        {
+            Boolean prevLoggerStatus = loggerStatus;
+            loggerStatus = false;
+            if (prevLoggerStatus == true)
+            {// if(searchForUser(newUserName)==false){
+                memberUsername = newUserName;
+                loggerStatus = prevLoggerStatus;
+                return true;
+            }
+
+            else return false;
+        }
+
+
     }
 }
