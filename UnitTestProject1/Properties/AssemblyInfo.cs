c11b9c16c6951f00e7333cfde1b46acae1686249
Tomer Belzer <iamtomer.b@gmail.com>
2015-04-19 21:05:37 +0300

    sakjcaskjx

--- UnitTestProject1/Properties/AssemblyInfo.cs
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6096bafc-cec9-41d7-ba9e-69eb7a046580")]
+[assembly: Guid("1e3dc8d6-cf20-4cee-92e1-69439adac063")]
 
 // Version information for an assembly consists of the following four values:
 //
