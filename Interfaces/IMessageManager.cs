91df2441ede7e31835a0b6c865778b214fa2a6fb
tomerse <tomerse@post.bgu.ac.il>
2015-04-19 23:53:43 +0300

    IMessageManager for Fibo

---
+++ Interfaces/IMessageManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IMessageManager
+    {
+        void addThread(int subForumId, int userID, string title, string body);
+        void addComment(int firstMessageId, int userID, string title, string body);
+        //void editMessage(int messageId, int userID, string title, string body);     //we don't have requirement for that
+        void deleteMessage(int messageId);
+    }
+}
