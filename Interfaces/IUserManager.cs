342d84e427d8d55dafc3469a2cc4c4909dd19785
poratGalpo <galpo@post.bgu.ac.il>
2015-04-20 12:05:50 +0300

    IUserInterface - comments were added

--- Interfaces/IUserManager.cs
+++ Interfaces/IUserManager.cs
@@ -8,17 +8,27 @@ namespace Interfaces
 {
     public interface IUserManager
     {
-        Boolean login(String username, String password);
-        Boolean logout(String username, String password);
-        int register(String username, String password);
+        int login(String username, String password); // returns the user's ID number,0 if the user already logged in
+
+        Boolean logout(int userID, String password); // returns false if the user is already offline, true otherwise
+
+        int register(String username, String password);// returns the user's ID number, 0 if the username is already taken
+
         Boolean enterForum(String forumName);// not sure that the right class for that
-        Boolean changePassword(String username, String oldPassword, String newPassword);
-        Boolean changeUsername(String oldUsername, String newUsername, String password);
-        Boolean addFriend(int userID ,int friendID);
+
+        Boolean changePassword(String username, String oldPassword, String newPassword);//returns false if the password is not legal
+
+        Boolean changeUsername(String oldUsername, String newUsername, String password);// returns false if the user name is taken
+
+        Boolean addFriend(int userID ,int friendID);// returns true if the friend request was sent, false otherwise
+ 
         void removeFriend(int friendID);
+
         void approveRequest(int notificationID);
-        //void complaint(notificationStatus complaint, int userID);
-        void deactivate();
-        //for upload
+
+        //void complaint(notificationStatus complaint, int userID);// notificationStatus yet to be written
+
+        void deactivate();// deletes the user account, user must be logged in
+      
     }
 }
