a0526088b6b60cbc63336b45fece0cc0c198ef51
poratGalpo <galpo@post.bgu.ac.il>
2015-04-20 11:17:11 +0300

    ..

---
+++ User/userManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    interface userManager
+    {
+        Boolean login(String username, String password);
+        int register(String username, String password);
+        Boolean enterForum(String forumName);// not sure that the right class for that
+        Boolean changePassword(String username, String oldPassword, String newPassword);
+        Boolean changeUsername(String oldUsername, String newUsername, String password);
+        Boolean addFriend(int userID ,int friendID);
+        void removeFriend(int friendID);
+        void approveRequest(int notificationID);
+        void complaint(notificationStatus complaint, int userID);
+        void deactivate();
+        //for upload
+    }
+}
