b6dd51002c1fa83b0da60612ba5d4a86d47a50af
Tomer Belzer <iamtomer.b@gmail.com>
2015-04-20 18:17:01 +0300

    tests - forum tests

--- testProject/UnitTest1.cs
+++ testProject/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace testProject
         String[] userNames = { "tomer.b", "tomer.s", "gal.b", "gal.p","osher"};
         String[] emails = { "tomer.b@gmail.com", "tomer.s@gmail.com", "gal.b@gmail.com", "gal.p@gmail.com", "osher@gmail.com" };
         String[] passwords = {"123456","abcdef"};
-        IUserManager um = UserManager.getInstance();
+        IUserManager um = new UserManager();
 
         /*
          * Testing regestration:
@@ -46,7 +46,7 @@ namespace testProject
         public void registerLoginLogoutTest()
         {
             int id1 = um.register(userNames[3], passwords[3], emails[3]);
-            Assert.AreEqual(um.login(userNames[0], passwords[0]), id1;
+            Assert.AreEqual(um.login(userNames[0], passwords[0]), id1);
             Assert.IsTrue(um.logout(id1));
         }
 
@@ -172,20 +172,103 @@ namespace testProject
         String[] titels = { "sport","nature"};
         String[] subTitels = { "football","basketball","animals","plants"};
         IForumManager fm = ForumManager.getInstance();
+        String[] user = { "tomer", "tomer@gmail.com", "123456" };
 
         /*
-         * Testing creating fotum:
+         * Testing creating forum:
          * creating forums eith two different titles returns two different IDs
-         * second regestration with same title returns -1
+         * second creation with same title returns -1
          */
         [TestMethod]
-        public void creatingForumTest()
+        public void creatingForumReturnsDiffIDTest()
         {
             int id1 = fm.createForum(titels[0]);
             int id2 = fm.createForum(titels[1]);
             int id3 = fm.createForum(titels[0]);
             Assert.AreNotEqual(id1, id2);
+        }
+
+        [TestMethod]
+        public void creatingForumWithExistTiltleFailTest()
+        {
+            int id3 = fm.createForum(titels[0]);
             Assert.AreEqual(id3, -1);
+        }
+
+        /*
+          * Testing creating forum:
+          * creating subforums with two different topics in same forum returns two different IDs
+          * second creation with same topic in same forum returns -1
+          */
+        [TestMethod]
+        public void creatingSubForumReturnsDiffIDTest()
+        {
+            int id1 = fm.getForumId(titels[0]);
+            int id2 = fm.createSubForum(subTitels[1], id1);
+            int id3 = fm.createSubForum(subTitels[0], id1);
+            Assert.AreNotEqual(id1, id2);
+        }
+
+        [TestMethod]
+        public void creatingSubForumWithExistTiltleFailTest()
+        {
+            int id1 = fm.getForumId(titels[0]);
+            int id3 = fm.createSubForum(subTitels[0], id1);
+            Assert.AreEqual(id3, -1);
+        }
+
+        /*
+         * testing adding and removal of admin to forum
+         */
+        [TestMethod]
+        public void addAdminTest()
+        {
+            int id1 = fm.getForumId(titels[0]);
+            int userId = fm.register(user[0], user[1], user[2], id1);
+            fm.addAdmin(userId, id1);
+            Assert.IsTrue(fm.isAdmin(userId,id1));
+            fm.removeAdmin(userId, id1);
+            fm.unRegister(userId, id1);
+        }
+
+        [TestMethod]
+        public void removeAdminTest()
+        {
+            int id1 = fm.getForumId(titels[0]);
+            int userId = fm.register(user[0], user[1], user[2], id1);
+            Assert.IsFalse(fm.isAdmin(userId, id1));
+            fm.addAdmin(userId, id1);
+            fm.removeAdmin(userId, id1);
+            Assert.IsFalse(fm.isAdmin(userId, id1));
+            fm.unRegister(userId, id1);
+        }
+
+        /*
+         * testing adding and removal of moderator to subforum
+         */
+        [TestMethod]
+        public void addModeratorTest()
+        {
+            int forumId = fm.getForumId(titels[0]);
+            int subForumId = fm.getSubForumId(forumId, subTitels[0]);
+            int userId = fm.register(user[0], user[1], user[2], forumId);
+            fm.addModerator(userId, forumId, subForumId);
+            Assert.IsTrue(fm.isModerator(userId, forumId, subForumId));
+            fm.removeModerator(userId, forumId, subForumId);
+            fm.unRegister(userId, forumId);
+        }
+
+        [TestMethod]
+        public void removeModeratorTest()
+        {
+            int forumId = fm.getForumId(titels[0]);
+            int subForumId = fm.getSubForumId(forumId, subTitels[0]);
+            int userId = fm.register(user[0], user[1], user[2], forumId);
+            Assert.IsFalse(fm.isModerator(userId, forumId, subForumId));
+            fm.addModerator(userId, forumId, subForumId);
+            fm.removeModerator(userId, forumId, subForumId);
+            Assert.IsFalse(fm.isModerator(userId, forumId, subForumId));
+            fm.unRegister(userId, id1);
         }
     }
 }
