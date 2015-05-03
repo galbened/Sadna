d6aabc0f27ccfd988e588ff1544ce0555bbe0a77
tomerse <tomerse@post.bgu.ac.il>
2015-05-03 23:49:43 +0300

    Interface for the user (use-cases)

---
+++ Interfaces/IApplication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IApplication
+    {
+      
+        public int CreateForum(int forumAdmin, string name, 
+                       int numOfModerators, string degreeOfEnsuring,
+                       Boolean uppercase, Boolean lowercase, Boolean numbers,
+                       Boolean symbols, int minLength);
+
+        public void SetPolicy(int numOfModerators, string degreeOfEnsuring,
+                       Boolean uppercase, Boolean lowercase, Boolean numbers,
+                       Boolean symbols, int minLength, int forumId);
+
+        //public void Entry(int forumId);
+
+        public int Register(string username, string password, string email);
+
+        public int Login(String username, String password, int forumId);
+
+        public bool Logout(int userId, int forumId);
+
+        public int CreateSubForum(string topic, int forumId, int callerUserId);
+
+        public void View(int forumId, int subForumId, out string[] subForumNames, out int[] subForumIds);
+
+        public int Publish(int forumId, int subForumId, int publisherID, string title, string body);
+
+        public int Comment(int firstMessageId, int publisherID, string title, string body);
+
+        public void SendFriendRequest();
+
+        public void ComplainModerator();
+
+        public bool DeleteMessage(int messageId);
+
+        public void RemoveForum(int forumId);
+
+        public void AddModerator(int forumId, int subForumId, int moderatorId);
+
+        public void RemoveModerator(int forumId, int subForumId, int moderatorId);
+
+
+
+
+
+    }
+}
