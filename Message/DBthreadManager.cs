2114792588910a687cfc40c4dcc7496893f1507a
galbened <galbened@post.bgu.ac.il>
2015-06-16 19:49:50 +0300

    DBthreadManager

---
+++ Message/DBthreadManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace Message
+{
+    class DBthreadManager : IDBManager<Thread>
+    {
+        Context db;
+
+        public DBthreadManager()
+        {
+            db = new Context();
+            //db.Database.ExecuteSqlCommand("TRUNCATE TABLE Members");
+        }
+
+        public Thread getObj(int ID)
+        {
+            return db.Threads.Find(ID);
+        }
+
+        public List<Thread> getAll()
+        {
+            return db.Threads.ToList();
+        }
+
+        public void update()
+        {
+            db.SaveChanges();
+        }
+
+
+        public void add(Thread obj)
+        {
+            db.Threads.Add(obj);
+        }
+
+
+        public void remove(Thread obj)
+        {
+            db.Threads.Remove(obj);
+        }
+    }
+}
