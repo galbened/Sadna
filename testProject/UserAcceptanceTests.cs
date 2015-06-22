381c8e4806ff9c9c8e7f9195d61fef2018b533dc
poratGalpo <galpo@post.bgu.ac.il>
2015-06-22 20:59:49 +0300

    user acceptance tests change

--- testProject/UserAcceptanceTests.cs
+++ testProject/UserAcceptanceTests.cs
@@ -42,10 +42,6 @@ namespace testProject
         [ClassCleanup]
         public static void TearDown()
         {
-            foreach(int mem in usersIds)
-            {
-                bridge.Deactivate(mem);
-            }
 
             bridge = null;
             usersIds = null;
@@ -90,7 +86,7 @@ namespace testProject
             int userId = bridge.Register(userNamesList[0], passwords[1], emails[0], forumId);
             Boolean ans = bridge.isUserRegistered(userId);
             Assert.IsTrue(ans);
-            usersIds.Add(userId);
+            //usersIds.Add(userId);
 
         }
 
